13a203b1e01b9c65c658b3652e4e89618d2ed9d3
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-16 04:42:25 +0000

    P5 done

--- p5final.c
+++ p5final.c
@@ -8,18 +8,20 @@ int input_array_size()
 }
 void init_array(int n,int a[n])
 {
-  for(int i=0;i<n;i++)
+  int i;
+  for(i=0;i<n;i++)
     {
       a[i]=i+1;
     }
 }
 void erotosthenes_sieve(int n, int a[n])
 {
-  for(int i=1;i<n;i++)
+  int i,j;
+  for( i=1;i<n;i++)
     {
-      if(a[i]=0)
+      if(a[i]==0)
         continue;
-      for(int j=a[i+1];j<=n;j++)
+      for(j=a[i]+1;j<=n;j++)
         {
           if(j%a[i]==0)
           {
@@ -30,11 +32,18 @@ void erotosthenes_sieve(int n, int a[n])
 }
 void out_put(int n, int a[n])
 {
-  
+  int i;
+  printf("Prime numbers are:\n");
+  for(i=0;i<n;i++)
+    printf("%d ",a[i]);
 }
 int main()
 {
   int n;
   n=input_array_size();
-  
+  int a[n];
+  init_array(n,a);
+  erotosthenes_sieve(n,a);
+  out_put(n,a);
+  return 0;
 }
