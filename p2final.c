13117826f69d8e69d6943c1a15894dd2b9ca6f1b
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-30 17:27:57 +0000

    P2 done

--- p2final.c
+++ p2final.c
@@ -3,7 +3,7 @@
 void input_triangle(float *x1,float *y1,float *x2,float *y2,float *x3,float *y3)
 {
   printf("Enter the three points of the triangle\n");
-  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+  scanf("%f%f %f%f %f%f",x1,y1,x2,y2,x3,y3);
 }
 float is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
 {
