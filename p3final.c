50a209b1b88df0a95289266515514c7d68b037b6
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-04-05 08:41:55 +0000

    ..

--- p3final.c
+++ p3final.c
@@ -1,34 +1,34 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 int input_number()
 {
   int n;
-  printf("Enter the number\n");
+  printf("Enter a number\n");
   scanf("%d",&n);
   return n;
 }
 int is_prime(int n)
 {
-  int i;
-  for(i=2;i<=sqrt(n);i++)
-  {
-    if(n%i==0);
-    return 0;
-  }
-  return 1;
+  int c=0,f=0,i;
+  for(i=1;i<=n;i++)
+    {
+      if(n%i == 0)
+      c++;
+    }
+  if(c>2)
+    f=1;
+  return f;
 }
-void output(int n,int is_prime)
+void output(int n, int is_prime)
 {
-  if(is_prime==0)
-    printf("%d is a prime number\n",n);
-  else
+  if(is_prime==1)
     printf("%d is not a prime number\n",n);
+  else
+    printf("%d is a prime number\n",n);
 }
 int main()
 {
-  int a,b;
-  a=input_number();
-  b=is_prime(a);
-  output(a,b);
+  int n=input_number();
+  int c=is_prime(n);
+  output(n,c);
   return 0;
 }
--- p7final.c
+++ p7final.c
@@ -10,13 +10,14 @@ Point p1,p2;
 float distance;
 };
 typedef struct _line Line;
-Point input_point(int p1,int p2)
+Point input_point()
 {
+  
   printf("Enter the values of x and y of both the points\n");
   scanf("%f%f %f%f",&p1.x,&p1.y,&p2.x,&p2.y);
   return p1;
 }
-Line input_line(l)
+Line input_line()
 {
   
 }
