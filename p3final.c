07674b8fa344652457c9429d504e321c9b693493
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-04-06 19:33:39 +0000

    ..

--- p3final.c
+++ p3final.c
@@ -8,22 +8,29 @@ int input_number()
 }
 int is_prime(int n)
 {
-  int c=0,f=0,i;
-  for(i=1;i<=n;i++)
+ // int c=0,f=0,i; 
+ // for(i=1;i<=n;i++)
+   // {
+     // if(n%i == 0)
+//      c++;
+ //   }
+//  if(c>2)
+  //  f=1;
+ // return f;
+  int i,c=0;
+  for(i=2;i<n;i++)
     {
-      if(n%i == 0)
-      c++;
+      if(n%i==0)
+        c++;
     }
-  if(c>2)
-    f=1;
-  return f;
+  return c;
 }
 void output(int n, int is_prime)
 {
-  if(is_prime==1)
-    printf("%d is not a prime number\n",n);
-  else
+  if(is_prime==0)
     printf("%d is a prime number\n",n);
+  else
+    printf("%d is not a prime number\n",n);
 }
 int main()
 {
